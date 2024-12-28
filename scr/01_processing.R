f98797aee56cacce1d00b9b4c76d7a7496a652fe
pedriniedoardo <pedrini.edoardo@gmail.com>
2024-12-28 01:53:47 +0100

    fix the filtering on QC

--- scr/01_processing.R
+++ scr/01_processing.R
@@ -1,6 +1,5 @@
 # AIM ---------------------------------------------------------------------
 # try to run harmony by merging the matrices from the individual objects. this will allow the skipping of the regular integration. the regular integration is needed as to run harmony the matrices should have the same number/order of the genes.
-# I will be skippint he QC steps as I already have access the the final whitelist of the barcodes retained in the final object
 
 # libraries ---------------------------------------------------------------
 library("Seurat")
@@ -132,8 +131,121 @@ sobj_total$percent.globin <- Seurat::PercentageFeatureSet(sobj_total,pattern = "
 # filter the whitelisted barcodes -----------------------------------------
 # according to the authors the dataset should be filtered using the following paramters:
 # Specifically, cells were first filtered to have at least 1000 UMIs, at least 100 genes and at most 10% mitochondrial gene expression
+sobj_total$test <- sobj_total@meta.data %>%
+  mutate(test = percent.mt < 10 & nFeature_RNA > 100 & nCount_RNA > 1000) %>% 
+  pull(test)
 
+# check the effect of the filters
+# extract the metadata from each dataset
+meta_total <- sobj_total@meta.data %>%
+  rownames_to_column("barcode")
+meta_total %>%
+  write_tsv("../../out/table/01_meta_datasc_beforeQC.tsv")
 
+# how many cells are considered outliers
+meta_total %>%
+  dplyr::count(orig.ident,test)
+
+# fixed threshold scatter nFeature vs percent.mt
+meta_total %>%
+  ggplot(aes(y = percent.mt,x = nFeature_RNA,col=test)) + geom_point(alpha=0.3) +
+  facet_wrap(~orig.ident) +
+  theme_bw() +
+  theme(strip.background = element_blank())
+# save the plot
+ggsave("../../out/plot/01_fixed_scatter_feature_mito.pdf",width = 8,height = 4)
+
+#
+meta_total %>%
+  gather(var,value,c(nCount_RNA,nFeature_RNA,percent.mt,percent.globin)) %>%
+  ggplot(aes(x=orig.ident,y=value)) +
+  geom_violin() +
+  geom_jitter(width = 0.2, alpha = 0.01) +
+  facet_wrap(~var,scales = "free") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45,hjust = 1)) +
+  theme(strip.background = element_blank())
+# save the plot
+ggsave("../../out/plot/01_fixed_boxplot_reads.pdf",width = 6,height = 6)
+
+#
+meta_total %>%
+  gather(var,value,c(nCount_RNA,nFeature_RNA,percent.mt,percent.globin)) %>%
+  filter(var == "percent.mt") %>%
+  ggplot(aes(x=value))+geom_histogram(binwidth = 0.05) +
+  facet_wrap(orig.ident~var,scales = "free") +
+  theme_bw() +
+  scale_x_continuous(trans = "log1p") +
+  geom_vline(xintercept = c(10),col="red",linetype="dashed") +
+  annotate("rect", xmin=0, xmax=10, ymin=0, ymax=Inf, alpha=0.1, fill="red")+
+  theme(strip.background = element_blank())
+# save the plot
+ggsave("../../out/plot/01_fixed_histo_mito.pdf",width = 8,height = 4)
+
+meta_total %>%
+  gather(var,value,c(nCount_RNA,nFeature_RNA,percent.mt,percent.globin)) %>%
+  filter(var == "nFeature_RNA") %>%
+  ggplot(aes(x=value)) + 
+  geom_histogram(binwidth = 0.05) +
+  facet_wrap(orig.ident~var,scales = "free") +
+  theme_bw() +
+  scale_x_continuous(trans = "log1p") +
+  geom_vline(xintercept = c(100),col="red",linetype="dashed") +
+  annotate("rect", xmin=100, xmax=Inf, ymin=0, ymax=Inf, alpha=0.1, fill="red")+
+  theme(strip.background = element_blank())
+# save the plot
+ggsave("../../out/plot/01_fixed_histo_features.pdf",width = 8,height = 4)
+
+#
+meta_total %>%
+  gather(var,value,c(nCount_RNA,nFeature_RNA,percent.mt,percent.globin)) %>%
+  filter(var == "nCount_RNA") %>%
+  ggplot(aes(x=value)) + 
+  geom_histogram(binwidth = 0.05) +
+  facet_wrap(orig.ident~var,scales = "free") +
+  theme_bw() +
+  scale_x_continuous(trans = "log1p") +
+  geom_vline(xintercept = c(1000),col="red",linetype="dashed") +
+  annotate("rect", xmin=1000, xmax=Inf, ymin=0, ymax=Inf, alpha=0.1, fill="red")+
+  theme(strip.background = element_blank())
+# save the plot
+ggsave("../../out/plot/01_fixed_histo_count.pdf",width = 8,height = 4)
+
+meta_total %>%
+  gather(var,value,c(nCount_RNA,nFeature_RNA,percent.mt,percent.ribo,percent.globin)) %>%
+  filter(var == "percent.ribo") %>%
+  ggplot(aes(x=value)) + 
+  geom_histogram(binwidth = 0.05) +
+  facet_wrap(orig.ident~var,scales = "free") +
+  theme_bw() +
+  scale_x_continuous(trans = "log1p") +
+  # geom_vline(xintercept = c(500,5000),col="red",linetype="dashed") +
+  # annotate("rect", xmin=500, xmax=5000, ymin=0, ymax=Inf, alpha=0.1, fill="red")+
+  theme(strip.background = element_blank())
+# save the plot
+ggsave("../../out/plot/fixed_histo_ribo.pdf",width = 8,height = 4)
+
+meta_total %>%
+  gather(var,value,c(nCount_RNA,nFeature_RNA,percent.mt,percent.ribo,percent.globin)) %>%
+  filter(var == "percent.globin") %>%
+  ggplot(aes(x=value)) + 
+  geom_histogram(binwidth = 0.05) +
+  facet_wrap(orig.ident~var,scales = "free") +
+  theme_bw() +
+  scale_x_continuous(trans = "log1p") +
+  # geom_vline(xintercept = c(500,5000),col="red",linetype="dashed") +
+  # annotate("rect", xmin=500, xmax=5000, ymin=0, ymax=Inf, alpha=0.1, fill="red")+
+  theme(strip.background = element_blank())
+# save the plot
+ggsave("../../out/plot/fixed_histo_globin.pdf",width = 8,height = 4)
+
+# color the bins for the amount of reads
+meta_total %>%
+  ggplot(aes(x = nCount_RNA,y = nFeature_RNA,col=mt_bin)) + geom_point(alpha=0.3) + facet_grid(orig.ident~mt_bin,scales = "free_y")+theme_bw() +
+  scale_x_continuous(labels = function(x) format(x, scientific = TRUE)) +
+  theme(strip.background = element_blank())
+# save the plot
+# ggsave("out/image/fixed_threshold/fixed_scatter_mito.pdf",width = 10,height = 6)
 
 # read in the full draft dataset
 # meta_whitelist <- LUT %>% 
@@ -175,19 +287,40 @@ sobj_total$percent.globin <- Seurat::PercentageFeatureSet(sobj_total,pattern = "
 # table(sobj_total@meta.data$whitelist)
 
 # apply the filtering on the whitelist
-# sobj_total <- subset(sobj_total,subset = whitelist == 1)
+sobj_total_filter <- subset(sobj_total,subset = test == 1)
+
+# what filter was the most critical
+sobj_total@meta.data %>%
+  mutate(test2 = percent.mt < 10 & nFeature_RNA > 100 & nCount_RNA > 1000) %>%
+  group_by(test2) %>%
+  summarise(n = n())
+
+sobj_total@meta.data %>%
+  mutate(test2 = percent.mt < 10) %>%
+  group_by(test2) %>%
+  summarise(n = n())
+
+sobj_total@meta.data %>%
+  mutate(test2 = nFeature_RNA > 100) %>%
+  group_by(test2) %>%
+  summarise(n = n())
+
+sobj_total@meta.data %>%
+  mutate(test2 = nCount_RNA > 1000) %>%
+  group_by(test2) %>%
+  summarise(n = n())
 
 # confirm the dimenstions
 # dim(sobj_total)
 
 # integration processing --------------------------------------------------
 # check the scale matrix
-sobj_total@assays$RNA
+sobj_total_filter@assays$RNA
 # pull all the genes to scale
 # all.genes <- rownames(sobj_total)
 
 # rescale the data for regressing out the sources of variation do not scale all the genes. if needed I can scale them before the heatmap call. for speeding up the computation I will keep 
-sobj_total <- sobj_total %>%
+sobj_total_filter <- sobj_total_filter %>%
   # skip the normalizatio that has been already performed at the beginning
   # Seurat::NormalizeData(verbose = T) %>%
   FindVariableFeatures(selection.method = "vst", nfeatures = 4000) %>%
@@ -202,24 +335,24 @@ sobj_total <- sobj_total %>%
   identity()
 
 # check the status of dataset preintegration
-DimPlot(sobj_total,group.by = "orig.ident",raster = T)
+DimPlot(sobj_total_filter,group.by = "orig.ident",raster = T)
 
 # Run Harmony -------------------------------------------------------------
 # The simplest way to run Harmony is to pass the Seurat object and specify which variable(s) to integrate out. RunHarmony returns a Seurat object, updated with the corrected Harmony coordinates. Let's set plot_convergence to TRUE, so we can make sure that the Harmony objective function gets better with each round.
-sobj_total_h <- sobj_total %>%
+sobj_total_filter_h <- sobj_total_filter %>%
   RunHarmony("orig.ident", plot_convergence = TRUE)
 # Harmony with two or more covariates
 # Do the same with your Seurat object:
 # seuratObject <- RunHarmony(seuratObject, c("dataset", "donor", "batch_id"))
 # To directly access the new Harmony embeddings, use the Embeddings command.
-harmony_embeddings <- Embeddings(sobj_total_h, 'harmony')
+harmony_embeddings <- Embeddings(sobj_total_filter_h, 'harmony')
 harmony_embeddings[1:5, 1:5]
 # Let's make sure that the datasets are well integrated in the first 2 dimensions after Harmony.
 # DimPlot(object = sobj_total_h, reduction = "harmony", pt.size = .1, group.by = "sample_id")
 
 # Downstream analysis -----------------------------------------------------
 # Many downstream analyses are performed on low dimensional embeddings, not gene expression. To use the corrected Harmony embeddings rather than PCs, set reduction = 'harmony'. For example, let's perform the UMAP and Nearest Neighbor analyses using the Harmony embeddings.
-sobj_total_h <- sobj_total_h %>%
+sobj_total_filter_h <- sobj_total_filter_h %>%
   RunUMAP(reduction = "harmony", dims = 1:30,return.model = TRUE) %>% 
   FindNeighbors(reduction = "harmony", dims = 1:30) %>% 
   FindClusters(resolution = seq(0.1, 1, by = 0.1)) %>%
@@ -227,16 +360,16 @@ sobj_total_h <- sobj_total_h %>%
   identity()
 
 # verify that all the relevant slots are filled
-sobj_total_h@assays$RNA$counts[1:20,1:10]
-sobj_total_h@assays$RNA$data[1:20,1:10]
-sobj_total_h@assays$RNA$scale.data[1:20,1:10]
+sobj_total_filter_h@assays$RNA$counts[1:20,1:10]
+sobj_total_filter_h@assays$RNA$data[1:20,1:10]
+sobj_total_filter_h@assays$RNA$scale.data[1:20,1:10]
 
-dim(sobj_total_h@assays$RNA$counts)
-dim(sobj_total_h@assays$RNA$data)
-dim(sobj_total_h@assays$RNA$scale.data)
+dim(sobj_total_filter_h@assays$RNA$counts)
+dim(sobj_total_filter_h@assays$RNA$data)
+dim(sobj_total_filter_h@assays$RNA$scale.data)
 
-DimPlot(sobj_total_h,group.by = "orig.ident",raster = F)
-DimPlot(sobj_total_h,split.by = "orig.ident",raster = F,ncol = 4)
+DimPlot(sobj_total_filter_h,group.by = "orig.ident",raster = F)
+DimPlot(sobj_total_filter_h,split.by = "orig.ident",raster = F,ncol = 4)
 
 # -------------------------------------------------------------------------
 # # add a costum annotation
@@ -249,4 +382,4 @@ DimPlot(sobj_total_h,split.by = "orig.ident",raster = F,ncol = 4)
 # -------------------------------------------------------------------------
 
 # sobj_total_h <- readRDS("../../out/object/data.combined_harmonySkipIntegration_AllSoupX_01000_6000_15.rds")
-saveRDS(sobj_total_h,"../../out/object/data.combined_harmonySkipIntegration.rds")
+saveRDS(sobj_total_filter_h,"../../out/object/data.combined_harmonySkipIntegration.rds")
